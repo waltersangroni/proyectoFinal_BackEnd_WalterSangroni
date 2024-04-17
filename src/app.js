cb62d30d5ff1fb5a7557c6f29c49a54605f7ed82
waltersangroni <walterhugosangroni@gmail.com>
2024-04-17 07:22:01 -0300

    Entrega desafio documentar api

--- src/app.js
+++ src/app.js
@@ -1,30 +1,3 @@
-// Realizar un sistema de recuperación de contraseña, la cual envíe por medio
-// de un correo un botón que redireccione a una página para restablecer la 
-// contraseña (no recuperarla).
-// link del correo debe expirar después de 1 hora de enviado.
-// Si se trata de restablecer la contraseña con la misma contraseña del usuario, 
-// debe impedirlo e indicarle que no se puede colocar la misma contraseña
-// Si el link expiró, debe redirigir a una vista que le permita generar 
-// nuevamente el correo de restablecimiento, el cual contará con una nueva 
-// duración de 1 hora.
-// Establecer un nuevo rol para el schema del usuario llamado “premium” el 
-// cual estará habilitado también para crear productos
-// Modificar el schema de producto para contar con un campo “owner”, 
-// el cual haga referencia a la persona que creó el producto
-// Si un producto se crea sin owner, se debe colocar por defecto “admin”.
-// El campo owner deberá guardar sólo el correo electrónico (o _id, lo 
-//   dejamos a tu conveniencia) del usuario que lo haya creado 
-//   (Sólo podrá recibir usuarios premium)
-// Modificar los permisos de modificación y eliminación de productos para que:
-// Un usuario premium sólo pueda borrar los productos que le pertenecen.
-// El admin pueda borrar cualquier producto, aún si es de un owner.
-// Además, modificar la lógica de carrito para que un usuario premium NO 
-// pueda agregar a su carrito un producto que le pertenece
-// Implementar una nueva ruta en el router de api/users, la cual 
-// será /api/users/premium/:uid  la cual permitirá cambiar el rol de un usuario, 
-// de “user” a “premium” y viceversa.
-
-
 import express from "express";
 //import ProductManager from "./dao/fileSystem/productManager.js"; 
 import ProductManager from "./dao/db/productManager.js"
@@ -39,10 +12,14 @@ import MongoStore from "connect-mongo";
 import sessionRoutes from "./routes/session.routes.js";
 import mongoose from "mongoose";
 import passport from "passport";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUiExpress from "swagger-ui-express";
+
 import  initializePassport from "./config/passport.config.js";
 import { mongoSecret, port, mongoUrl } from "./config/env.config.js"
 import { ErrorHandler } from "./middlewars/error.js";
 import { addLogger } from "./utils/logger.js";
+import { swaggerConfig } from "./configs/swagger-config.js";
 
 dbConnect();
 
@@ -51,6 +28,9 @@ const productManager = new ProductManager();
 await productManager.init();
 const PORT= port;
 const app = express();
+
+const specs = swaggerJSDoc(swaggerConfig);
+app.use("/apidocs", swaggerUiExpress.serve, swaggerUiExpress.setup(specs));
 
 app.use(session({
   secret: mongoSecret,
---
+++ src/config/swagger-config.js
@@ -0,0 +1,10 @@
+export const swaggerConfig = {
+    definition: {
+        openapi: "3.0.1",
+        info: {
+            title: "Documentación API Ecommerce Backend",
+            description: "Documentación API para proyecto final de BackEnd en CoderHouse"
+        }
+    },
+    apis: ["src/docs/**/*.yaml"]
+}
